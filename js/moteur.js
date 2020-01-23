2fdcd27f82a53cfc60134df4a91e87e582937ee2
castano cindy <castano.cindy1@gmail.com>
2020-01-23 22:21:49 +0100

    Desktop + tablet

--- js/moteur.js
+++ js/moteur.js
@@ -3,17 +3,87 @@
 /* jslint expr: true */
 
 
-/* ============================= SCROLL ANIMATION ================================ */
+/* ============================= SCROLL ANIMATION HOUSE ================================ */
     
 var revealElements = document.getElementsByClassName("digit");
-for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+for (var i=0; i<revealElements.length; i++) {
 new ScrollMagic.Scene({
-    triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-    offset: 50,												 // start a little later
+    triggerElement: revealElements[i],
+    offset: 50,
     triggerHook: 0.9,
 })
-.setClassToggle(revealElements[i], "visible") // add class toggle
-.addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
+.setClassToggle(revealElements[i], "visible")
+.addIndicators({name: "digit " + (i+1) })
 .addTo(controller);
 }
-    
+
+/* ============================= SCROLL ANIMATION NUMBERS ================================ */
+
+var revealNum = document.getElementsByClassName("revealNumber");
+for (var i=0; i<revealNum.length; i++) {
+new ScrollMagic.Scene({
+    triggerElement: revealNum[i],
+    offset: 50,
+    triggerHook: 0.9,
+    })
+.setClassToggle(revealNum[i], "visible")
+.addIndicators({name: "revealNumber " + (i+1) })
+.addTo(controller);
+}
+
+/* ============================= SCROLL ANIMATION VALEURS ================================ */
+
+new ScrollMagic.Scene({
+    triggerElement: "#valeurs",
+    triggerHook: 0.9,
+    duration: "80%",
+    offset: 50
+})
+.setClassToggle(".revealItem", "visible")
+.addIndicators()
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#qualifications",
+    triggerHook: 0.9,
+    duration: "80%",
+    offset: 50
+})
+.setClassToggle(".revealCard", "visible")
+.addIndicators()
+.addTo(controller);
+
+/* ============================= DROPDOWN LIST ================================ */
+
+function slideMenu() {
+    $(".climList").toggleClass("visible");
+}
+function dropMenu() {
+    $(".chauf_list").toggleClass("visible");
+}
+
+function downMenu() {
+    $(".vent_list").toggleClass("visible");
+}
+
+$(".dropClim").click(function () {
+    slideMenu();
+});
+
+$(".dropChauf").click(function () {
+    dropMenu();
+});
+
+$(".dropVent").click(function () {
+    downMenu();
+});
+
+
+
+
+$("#scrollTop").click(function(){
+    $("body,html").animate(
+        {
+            scrollTop : 0
+        }, 400);
+});
